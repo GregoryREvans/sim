7fb69f7d99f0fd2416b5479bd73da8c5ad19a3d2
GregoryREvans <gregoryrowlandevans@gmail.com>
2020-05-16 20:25:30 -0400

    Initializers.

--- sim/Materials/timespans/Segment_I/convert_timespans.py
+++ sim/Materials/timespans/Segment_I/convert_timespans.py
@@ -10,7 +10,6 @@ from sim.Materials.score_structure.Segment_I.dynamic_material_pattern import (
 from sim.Materials.score_structure.Segment_I.pitch_material_pattern import (
     pitch_material_list,
 )
-# from evans.abjad_functions.ConvertTimespans import ConvertTimespans
 from sim.Materials.score_structure.Segment_I.rhythm_material_pattern import (
     rhythm_material_list,
 )
