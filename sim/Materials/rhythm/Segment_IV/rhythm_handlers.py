ded7c8ffd8df82521bb3f83ae4421d7bd376ffa3
GregoryREvans <gregoryrowlandevans@gmail.com>
2020-05-29 11:04:51 -0400

    Mypy.

--- sim/Materials/rhythm/Segment_IV/rhythm_handlers.py
+++ sim/Materials/rhythm/Segment_IV/rhythm_handlers.py
@@ -3,12 +3,12 @@ import abjadext.rmakers
 import evans
 from sim.Materials.rhythm.Segment_IV.rhythm_makers import rmaker_one
 
-silence_maker = abjadext.rmakers.stack(
+silence_maker_ = abjadext.rmakers.stack(
     abjadext.rmakers.NoteRhythmMaker(),
     abjadext.rmakers.force_rest(abjad.select().leaves(pitched=True)),
 )
 
-silence_maker = evans.RhythmHandler(rmaker=silence_maker, name="silence maker")
+silence_maker = evans.RhythmHandler(rmaker=silence_maker_, name="silence maker")
 
 rhythm_handler_one = evans.RhythmHandler(
     rmaker=rmaker_one, continuous=True, name="rhythm_handler_one"
--- sim/Materials/score_structure/instruments.py
+++ sim/Materials/score_structure/instruments.py
@@ -2,23 +2,31 @@ import abjad
 
 instrument_one = abjad.Piano()
 instrument_one_range = instrument_one.pitch_range
-instrument_one_range_lowest = abjad.NumberedPitch(0)
-instrument_one_range_highest = abjad.NumberedPitch(instrument_one_range.stop_pitch)
+instrument_one_range_lowest = 0
+instrument_one_range_highest = abjad.NumberedPitch(
+    instrument_one_range.stop_pitch
+).number
 
 instrument_two = abjad.Piano()
 instrument_two_range = instrument_two.pitch_range
-instrument_two_range_lowest = abjad.NumberedPitch(0)
-instrument_two_range_highest = abjad.NumberedPitch(instrument_two_range.stop_pitch)
+instrument_two_range_lowest = 0
+instrument_two_range_highest = abjad.NumberedPitch(
+    instrument_two_range.stop_pitch
+).number
 
 instrument_three = abjad.Piano()
 instrument_three_range = instrument_three.pitch_range
-instrument_three_range_lowest = abjad.NumberedPitch(instrument_three_range.start_pitch)
-instrument_three_range_highest = abjad.NumberedPitch(6)
+instrument_three_range_lowest = abjad.NumberedPitch(
+    instrument_three_range.start_pitch
+).number
+instrument_three_range_highest = 6
 
 instrument_four = abjad.Piano()
 instrument_four_range = instrument_four.pitch_range
-instrument_four_range_lowest = abjad.NumberedPitch(instrument_four_range.start_pitch)
-instrument_four_range_highest = abjad.NumberedPitch(6)
+instrument_four_range_lowest = abjad.NumberedPitch(
+    instrument_four_range.start_pitch
+).number
+instrument_four_range_highest = 6
 
 instruments = [instrument_one, instrument_two, instrument_three, instrument_four]
 voices = len(instruments)
