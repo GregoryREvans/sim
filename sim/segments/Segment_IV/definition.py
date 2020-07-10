353e461141435ac48335e3173fa506a1fb515c6e
GregoryREvans <gregoryrowlandevans@gmail.com>
2020-07-10 11:08:22 -0400

    Build path.

--- sim/segments/Segment_IV/definition.py
+++ sim/segments/Segment_IV/definition.py
@@ -252,11 +252,10 @@ maker = evans.SegmentMaker(
     add_final_grand_pause=False,
     score_includes=[
         "/Users/evansdsg2/abjad/docs/source/_stylesheets/abjad.ily",
-        "/Users/evansdsg2/Scores/sim/sim/Build/first_stylesheet.ily",
+        "/Users/evansdsg2/Scores/sim/sim/build/first_stylesheet.ily",
     ],
     segment_name="Segment_IV",
-    current_directory=pathlib.Path(__file__).parent,
-    build_path=(pathlib.Path(__file__).parent / ".." / ".." / "Build").resolve(),
+    current_directory=pathlib.Path(__file__).resolve().parent,
     cutaway=False,
     beam_pattern="meter",
     beam_rests=False,
